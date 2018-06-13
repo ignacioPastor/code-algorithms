455bd44b9cceda21c28e4179c2b0bc0a2391b98e
ignacioPastor <ignacio.pastor@clearpeaks.com>
2018-06-13 22:07:34 +0200

    validBraces

---
+++ src/validBraces/validBraces.js
@@ -0,0 +1,19 @@
+function validBraces(braces) {
+  let pairs = ['()', '[]', '{}']
+  let mod;
+
+  do {
+    mod = false;
+    pairs.forEach(pair => {
+      if (braces.indexOf(pair) > -1) {
+        braces = braces.replace(pair, '');
+        mod = true;
+      }
+    });
+  } while (mod === true)
+
+  return braces === '';
+}
+
+console.log(validBraces("()"));
+console.log(validBraces("[(])"));
