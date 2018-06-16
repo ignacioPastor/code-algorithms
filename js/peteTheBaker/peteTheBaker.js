82074489f627f62a97628b400e2f764515d20493
ignacioPastor <ignacio.pastor@clearpeaks.com>
2018-06-16 12:59:43 +0200

    stable working

---
+++ js/peteTheBaker/peteTheBaker.js
@@ -0,0 +1,18 @@
+function cakes(recipe, available) {
+    let amountPies = -1;
+
+    Object.keys(recipe).forEach(ingredient => {
+        let nPieIng = (available[ingredient] || 0) / recipe[ingredient];
+
+        if (amountPies === -1 || nPieIng < amountPies) {
+            amountPies = nPieIng;
+        }
+    });
+
+    return Math.trunc(amountPies);
+}
+
+// must return 2
+console.log(cakes({ flour: 500, sugar: 200, eggs: 1 }, { flour: 1200, sugar: 1200, eggs: 5, milk: 200 }));
+// must return 0
+console.log(cakes({ apples: 3, flour: 300, sugar: 150, milk: 100, oil: 100 }, { sugar: 500, flour: 2000, milk: 2000 }));
