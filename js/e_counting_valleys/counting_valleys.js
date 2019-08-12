3fdb6edb76f76f431fd946b0db53e00fdbf2375f
Ignacio Pastor <ignacio.pastor@clearpeaks.com>
2019-08-12 17:11:20 +0400

    feat: new algorithm implemented

---
+++ js/e_counting_valleys/counting_valleys.js
@@ -0,0 +1,15 @@
+function countingValleys(n, s) {
+  let nValleys = 0;
+  [...s].reduce((previousElevation, step) => {
+      const currentElevation = previousElevation + (step === 'D' ? -1 : 1);
+      if (previousElevation === 0 && currentElevation === -1) {
+          ++nValleys;
+      }
+      return currentElevation;
+  }, 0);
+  return nValleys;
+}
+
+// Testing section
+console.log(countingValleys(8, 'UDDDUDUU')); // Expected 1
+console.log(countingValleys(12, 'DDUUDDUDUUUD')); // Expected 2
