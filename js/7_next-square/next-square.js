c5fdaa0885156e8cdbf7aa0c6d62cb321fd32f93
Ignacio Pastor <ignacio.pastor@clearpeaks.com>
2019-09-30 00:39:03 +0400

    refac: added testing library to next-square

--- js/7_next-square/next-square.js
+++ js/7_next-square/next-square.js
@@ -1,18 +1,8 @@
-function findNextSquare(sq) {
+const findNextSquare = sq => {
   const square = Math.sqrt(sq);
   return Number.isInteger(square) ? Math.pow(square + 1, 2) : -1;
 }
 
-
-// must return 144
-console.log(findNextSquare(121));
-// must return 676
-console.log(findNextSquare(625));
-// must return 320356
-console.log(findNextSquare(319225));
-// must return 15241630849
-console.log(findNextSquare(15241383936));
-// must return -1
-console.log(findNextSquare(155));
-// must return -1
-console.log(findNextSquare(342786627));
+module.exports = {
+  findNextSquare
+}
