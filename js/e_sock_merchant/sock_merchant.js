81e1dcdee537901f1ea08ffe6cfe5211589971a8
Ignacio Pastor <ignacio.pastor@clearpeaks.com>
2019-09-30 00:57:11 +0400

    refac: added testint library sock_merchant

--- js/e_sock_merchant/sock_merchant.js
+++ js/e_sock_merchant/sock_merchant.js
@@ -1,24 +1,16 @@
-const { getArrNumber } = require('./../utils');
-
-function sockMerchant(n, ar) {
+const sockMerchant = (n, ar) => {
   const occurrences = [...new Set(ar)];
   const countPairsMap = {};
   ar.forEach(val => {
-      if (occurrences.indexOf(val) > -1) {
-          countPairsMap[val] = (countPairsMap[val] || 0) + 1
-      }
+    if (occurrences.indexOf(val) > -1) {
+      countPairsMap[val] = (countPairsMap[val] || 0) + 1
+    }
   });
   return Object.values(countPairsMap).reduce((acc, val) => {
-      return acc + Math.floor(val / 2);
+    return acc + Math.floor(val / 2);
   }, 0);
+};
 
+module.exports = {
+  sockMerchant
 }
-
-
-// Testing section
-
-const test0 = getArrNumber('10 20 20 10 10 30 50 10 20');
-console.log(sockMerchant(9, test0)); // Expected 3
-
-const test1 = getArrNumber('1 1 1 2 2 3 4 5 4 6 7 6 6 1 7 9 9 12 11');
-console.log(sockMerchant(4234, test1)); // Expected 7
